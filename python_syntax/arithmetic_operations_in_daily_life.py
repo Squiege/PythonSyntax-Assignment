5c98b62a1217104184dab086dd7f6ea99f8e4632
Devin Rider <drider539@gmail.com>
2024-08-08 17:08:43 -0700

    Python Syntax Assignment

---
+++ python_syntax/arithmetic_operations_in_daily_life.py
@@ -0,0 +1,18 @@
+# Task 1 - Calculate the total cost of three items commonly found in grocery store
+apple = .99
+crackers = 2.49
+cheese = 4.99
+
+print("Grocery Receipt:")
+print("Apple - ", apple)
+print("Crackers - ", crackers)
+print("Cheese - ", cheese)
+
+# Task 2 - Interest Rate
+savings_amount = 10000
+interest_rate = .07
+interest_math = savings_amount * interest_rate
+total = savings_amount + interest_math
+print("By the end of the year you would have ", total)
+
+print("The amount you would have at the end of the year is:")
---
+++ python_syntax/showcase_your_dance_moves.py
@@ -0,0 +1,16 @@
+# Task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+#Task 2
+x = input("how are we feeling today?")
+if x == "good":
+    print("That's great to hear!")
+elif x == "sad":
+    print("I hope your day gets better!")
+else:
+    print("")
